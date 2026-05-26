1b7fdc9fb37d89926ef8f6a2c8545272e6551b6f
Taro Matsuzawa aka. btm <btm@tech.email.ne.jp>
2026-05-27 08:20:57 +0900

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
+  base: '/omt-router-demo/',
   resolve: {
     alias: {
       'omt-router/dist/omt-router.css': fileURLToPath(
